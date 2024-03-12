54a9835d3b465a507128185ccce3a8b2a52e96fe
4kbr <igyutubfesbuk@gmail.com>
2024-03-12 21:16:13 +0700

    set and get cookie

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -8,11 +8,22 @@ import {
   Query,
   Redirect,
   Req,
+  Res,
 } from '@nestjs/common';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 
 @Controller('/api/users')
 export class UserController {
+  @Get(`/set-cookie`)
+  setCookie(@Query(`name`) name: string, @Res() response: Response) {
+    response.cookie(`name`, name);
+    response.status(200).send(`Success Set Cookie`);
+  }
+  @Get(`/get-cookie`)
+  getCookie(@Req() request: Request): string {
+    return request.cookies['name'];
+  }
+
   @Get('/hello-promise')
   async sayHelloPromise(
     @Query('first_name') firstName?: string,
