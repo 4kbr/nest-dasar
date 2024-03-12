a584c2003a27cb00a8350299b1ba6c8cb8e0de41
4kbr <igyutubfesbuk@gmail.com>
2024-03-12 21:50:44 +0700

    integration test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,16 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('should return say hello', async () => {
+    const result = await request(app.getHttpServer())
+      .get(`/api/users/hello`)
+      .query({
+        first_name: `Oke`,
+        last_name: `Mantap`,
+      });
+
+    expect(result.status).toBe(200);
+    expect(result.text).toBe(`Hello Oke Mantap`);
+  });
 });
