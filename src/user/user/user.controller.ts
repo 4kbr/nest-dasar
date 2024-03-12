02cc6f8a8e062463eadd77cab3082e2c86a1ace1
4kbr <igyutubfesbuk@gmail.com>
2024-03-12 21:03:59 +0700

    http

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,4 +1,67 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpRedirectResponse,
+  Post,
+  Query,
+  Redirect,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('/api/users')
-export class UserController {}
+export class UserController {
+  @Get('/hello-promise')
+  async sayHelloPromise(
+    @Query('first_name') firstName?: string,
+    @Query('last_name') lastName?: string,
+  ): Promise<string> {
+    return `GET ${firstName + ' ' + lastName}`;
+  }
+
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse() {
+    return {
+      data: `Hello JSON`,
+    };
+  }
+  // sampleResponse(@Res() response: Response) {
+  //   response.status(200).json({ oke: 'mantap' });
+  // }
+
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      url: '/api/users/sample-response',
+      statusCode: 301,
+    };
+  }
+
+  @Post()
+  post(): string {
+    return 'POST';
+  }
+
+  @Get('/sample')
+  get() {
+    return 'Hello World';
+  }
+
+  @Get('/hello')
+  sayHello(
+    @Query('first_name') firstName?: string,
+    @Query('last_name') lastName?: string,
+  ): string {
+    return `GET ${firstName + ' ' + lastName}`;
+  }
+
+  @Get('/:id')
+  getById(@Req() request: Request): string {
+    return `GET ${request.params.id}`;
+  }
+}
